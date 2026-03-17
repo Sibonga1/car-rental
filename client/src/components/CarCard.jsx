2ce2c2ae7a952ca3319a4db20d14ae05db6e20df
Sibonga1 <sibongasiyanda0@gmail.com>
2026-03-17 18:39:59 +0200

    Enhance CarCard component with navigation and fix availability property

--- client/src/components/CarCard.jsx
+++ client/src/components/CarCard.jsx
@@ -1,51 +1,66 @@
 import React from "react";
 import { assets } from "../assets/assets";
+import { useNavigate } from "react-router-dom";
 
 const CarCard = ({ car }) => {
-
-    const currency = import.meta.env.VITE_CURRENCY || "R"; // Access the currency symbol from environment variables, defaulting to "$" if not set
+  const navigate = useNavigate();
+  const currency = import.meta.env.VITE_CURRENCY || "R"; // Access the currency symbol from environment variables, defaulting to "$" if not set
   return (
-    <div className="group rounded-xl overflow-hidden shadow-lg hover:-translate-y-1 transition-all duration-500 cursor-pointer">
+    <div
+      className="group rounded-xl overflow-hidden shadow-lg hover:-translate-y-1 
+    transition-all duration-500 cursor-pointer"
+      onClick={() => {
+        navigate(`car-Details/${car._id}`);
+        scrollTo(0, 0);
+      }}
+    >
       <div className="relative h-48 overflow-hidden">
         <img
           src={car.image}
           alt="car image"
           className="w-full h-full object-cover transition-transform duration-500 group-hover:scale-105"
         />
-        {car.isAvaliable && (
+        {car.isAvailable && (
           <p className="absolute top-4 bg-primary/90 text-white text-xs px-2.5 py-1 rounded-full">
             Available Now
           </p>
         )}
         <div className="absolute bottom-4 right-4 bg-black/80 backdrop-blur-sm text-white px-3 py-2 rounded-lg">
-          <span className="font-semibold">{currency}{car.pricePerDay}</span>
+          <span className="font-semibold">
+            {currency}
+            {car.pricePerDay}
+          </span>
           <span className="text-sm text-white/80"> / day</span>
         </div>
       </div>
       <div className="p-4 sm:p-5">
         <div className="flex justify-between items-start mb-2">
-            <div>
-                <h3 className="text-lg font-medium">{car.brand} {car.model}</h3>
-                <p className="text-muted-foreground text-sm">{car.category} {car.year}</p>
-            </div>
+          <div>
+            <h3 className="text-lg font-medium">
+              {car.brand} {car.model}
+            </h3>
+            <p className="text-muted-foreground text-sm">
+              {car.category} {car.year}
+            </p>
+          </div>
         </div>
         <div className="mt-4 grid grid-cols-2 gap-y-2 text-gray-600">
-            <div className="flex items-center text-sm text-muted-foreground">
-                <img src={assets.users_icon} alt="" className="h-4 mr-2"/>
-                <span>{car.seating_capacity} Seats</span>
-            </div>
-            <div className="flex items-center text-sm text-muted-foreground">
-                <img src={assets.fuel_icon} alt="" className="h-4 mr-2"/>
-                <span>{car.fuel_type}</span>
-            </div>
-            <div className="flex items-center text-sm text-muted-foreground">
-                <img src={assets.car_icon} alt="" className="h-4 mr-2"/>
-                <span>{car.transmission}</span>
-            </div>
-            <div className="flex items-center text-sm text-muted-foreground">
-                <img src={assets.location_icon} alt="" className="h-4 mr-2"/>
-                <span>{car.location}</span>
-            </div>
+          <div className="flex items-center text-sm text-muted-foreground">
+            <img src={assets.users_icon} alt="" className="h-4 mr-2" />
+            <span>{car.seating_capacity} Seats</span>
+          </div>
+          <div className="flex items-center text-sm text-muted-foreground">
+            <img src={assets.fuel_icon} alt="" className="h-4 mr-2" />
+            <span>{car.fuel_type}</span>
+          </div>
+          <div className="flex items-center text-sm text-muted-foreground">
+            <img src={assets.car_icon} alt="" className="h-4 mr-2" />
+            <span>{car.transmission}</span>
+          </div>
+          <div className="flex items-center text-sm text-muted-foreground">
+            <img src={assets.location_icon} alt="" className="h-4 mr-2" />
+            <span>{car.location}</span>
+          </div>
         </div>
       </div>
     </div>
