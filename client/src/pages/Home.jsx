40daebed8828eb6294c27befbeb8d9951c422c3a
Sibonga1 <sibongasiyanda0@gmail.com>
2026-03-17 18:42:05 +0200

    Add FeaturedSection, Banner, Testimonial, and Newsletter components to Home page

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,14 +1,20 @@
-import React from 'react'
-import Hero from '../components/Hero'
-import CarCard from '../components/CarCard'
+import React from "react";
+import Hero from "../components/Hero";
+import FeaturedSection from "../components/FeaturedSection";
+import Banner from "../components/Banner";
+import Testimonial from "../components/Testimonial";
+import Newsletter from "../components/Newsletter";
 
 const Home = () => {
   return (
     <>
       <Hero />
-     
+      <FeaturedSection />
+      <Banner />
+      <Testimonial />
+      <Newsletter />
     </>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
