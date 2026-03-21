1be0c895b1a332cba9af507b8df43ab66b0adfe4
Sibonga1 <sibongasiyanda0@gmail.com>
2026-03-21 16:37:28 +0200

    Implement CarDetails component to display car information and loading state

--- client/src/pages/CarDetails.jsx
+++ client/src/pages/CarDetails.jsx
@@ -1,9 +1,25 @@
-import React from 'react'
+import React from "react";
+import { useNavigate, useParams } from "react-router-dom";
+import { assets, dummyData } from "../assets/assets";
+import { useEffect, useState } from "react";
 
 const CarDetails = () => {
-  return (
-    <div>CarDetails</div>
-  )
+  const { id } = useParams();
+  const navigate = useNavigate();
+  const [car, setCar] = useState(null);
+
+  useEffect(() => {
+    setCar(dummyData.find(car => car._id === id));
+  }, [id]);
+
+return car ? (
+<div className="px-6 md:px-12 lg:px-24 xl:px-32 mt-16">
+<button>
+  <img src={assets.arrow_icon} alt="" className="rotate-180 opacity-65"/>
+  Back to all cars
+</button>
+</div>
+) : <p>Loading......</p>
 }
 
-export default CarDetails
+export default CarDetails;
