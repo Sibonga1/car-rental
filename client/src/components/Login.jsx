8e3e1a5470417da67c4b74960a5346c9d4c73439
Sibonga1 <sibongasiyanda0@gmail.com>
2026-04-24 12:51:04 +0200

    completed LOGIN signUp Page

--- client/src/components/Login.jsx
+++ client/src/components/Login.jsx
@@ -16,8 +16,11 @@ const Login = ({ setShowLogin }) => {
       onClick={() => setShowLogin(false)}
       className="fixed top-0 left-0 right-0 bottom-0 z-100 flex items-center text-sm text-gray-600 bg-black/60"
     >
-
-      <form onSubmit={onSubmitHandler} onClick={(e) => e.stopPropagation()} className="flex flex-col gap-4 m-auto items-start p-8 py-12 w-80 sm:w-[352px] text-gray-500 rounded-lg shadow-xl border border-gray-200 bg-white">
+      <form
+        onSubmit={onSubmitHandler}
+        onClick={(e) => e.stopPropagation()}
+        className="flex flex-col gap-4 m-auto items-start p-8 py-12 w-80 sm:w-[352px] text-gray-500 rounded-lg shadow-xl border border-gray-200 bg-white"
+      >
         <p className="text-2xl font-medium m-auto">
           <span className="text-primary">User</span>{" "}
           {state === "login" ? "Login" : "Sign Up"}
